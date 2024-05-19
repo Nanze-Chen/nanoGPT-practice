9d98af2e897af69335316f6d78aebeac205353ba
Nanze Chen <curtis_chen@outlook.com>
2024-05-19 17:08:32 +0100

    Add multi-head layer and a single feedforward layer

--- bigram.py
+++ bigram.py
@@ -92,6 +92,16 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([head(x) for head in self.heads], dim=-1)
 
+class FeedWard(nn.Module):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(in_size, out_size),
+            nn.ReLU(),
+        )
+    
+    def forward(self,x):
+        return self.net(x)
 
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
@@ -101,6 +111,7 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.sa_head = MultiHeadAttention(4, n_embd//4)     #i.e. 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedWard(n_embd, n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, target=None):
@@ -110,6 +121,7 @@ class BigramLanguageModel(nn.Module):
         pos_embd = self.position_embedding_table(torch.arange(T, device=device))   # (T, n_embd)
         x = token_embd + pos_embd           #(B,T,n_embd)
         x = self.sa_head(x)                 #apply one head self-attention. (B,T,C)
+        x = self.ffwd(x)                    #(B,T,C)
         logits = self.lm_head(x)            #(B,T,vocab_size)
 
         if target == None:
